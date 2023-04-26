68c3099a26d552e523147af7e7f83999058b7f94
antonionicolaci <antonionicolaci48@gmail.com>
2023-04-26 17:09:14 +0200

    add  16 different random numbers

--- script.js
+++ script.js
@@ -4,6 +4,33 @@ function tabella(){
     let nCasella = 0;
     const altezzaLunghezza = document.getElementById("diff").value;
     let stampFinale = "";
+    let numBombe = [];
+
+    for(let i = 0; 16 > i; i++){
+        numBombe[i] = randomMaxMin(1, altezzaLunghezza*altezzaLunghezza);
+    }
+    
+    let numDiversi = 0;
+    do {
+        for (let i = 0; i < 16; i++) {
+            let k = 0;
+            let x = 0;
+            for (let j = 0; i > j; j++) {
+                if (numBombe[i] == numBombe[j]) {
+                    x++;
+                }
+            }
+
+            if (x != 0 || x != 1){
+                numBombe[i] = randomMaxMin(1, altezzaLunghezza*altezzaLunghezza);
+                numDiversi++;
+            }
+        }
+            
+    } while (numDiversi == 0);
+    console.log(numBombe);
+
+
 
     for(let i = 0; altezzaLunghezza > i; i++){
         stampFinale += `<div class="riga">`;
@@ -20,4 +47,8 @@ function tabella(){
 function casella(numeroCasella){
     document.getElementById(`casella${numeroCasella}`).className += " evidenziato";
     console.log(numeroCasella);
+}
+
+function randomMaxMin(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
